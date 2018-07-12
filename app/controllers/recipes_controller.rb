d54f9b04b359bf3bd599871689eedd753f8e2694
Karl Amoguis <ktamoguis@gmail.com>
2018-07-12 06:22:26 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,8 +18,8 @@ class RecipesController < ApplicationController
   end
 
   private
- 
-  def person_params
-    params.require(:person).permit(:name)
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
   end
 end
